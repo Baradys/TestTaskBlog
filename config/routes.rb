af0318262932b76aefae5dc00cf79abd0a2bc98c
Mikhail Bratukhin <bym97m@mail.ru>
2022-07-18 17:58:06 +0300

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,9 @@
 Rails.application.routes.draw {
-  get '/users', to: 'users#users'
-
-  get '/users/:id', to: 'users#user_id'
-
-  post '/users', to: 'users#create'
-
-  put '/users/:id', to: 'users#update'
-
-  delete 'users/:id', to: 'users#destroy'
-
-  post '/posts', to: 'posts#create'
-
-  put '/posts/:id', to: 'posts#update'
-
-  delete 'posts/:id', to: 'posts#destroy'
-
-  post '/comments', to: 'comments#create'
-
-  delete 'comments/:id', to: 'comments#destroy'
+  Rails.application.routes.draw do
+    resources :users
+    resources :posts, only: %i[create update destroy]
+    resources :comments, only: %i[create destroy]
+  end
 
   root 'users#users'
 }
